f4eb99d9b38081a2c8dfedb7981a9a8f2de799c4
b20089 <e1b20089@oit.ac.jp>
2022-11-08 15:29:37 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/team10/kaizi/domino/DominoApplication.java
@@ -0,0 +1,13 @@
+package oit.is.team10.kaizi.domino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/team10/kaizi/domino/DominoApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.team10.kaizi.domino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
