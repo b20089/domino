b5974081285dec8fb1c1141be9ed02f597544b09
kanikama334 <kamo02903@gmail.com>
2022-11-15 15:21:43 +0900

    DataBaseの追加とモデル化の処理

--- src/main/java/oit/is/team10/kaizi/domino/controller/dominoController.java
+++ src/main/java/oit/is/team10/kaizi/domino/controller/dominoController.java
@@ -6,9 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import oit.is.team10.kaizi.domino.model.RoomMapper;
+import oit.is.team10.kaizi.domino.model.UserMapper;
 
 @Controller
 public class dominoController {
+
+  @Autowired
+  RoomMapper roomMapper;
+
+  @Autowired
+  UserMapper userMapper;
 
   @GetMapping("/domino")
   public String sample21() {
---
+++ src/main/java/oit/is/team10/kaizi/domino/model/Room.java
@@ -0,0 +1,32 @@
+package oit.is.team10.kaizi.domino.model;
+
+public class Room {
+  int id;
+  int userid;
+  int dominos;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getUserid() {
+    return userid;
+  }
+
+  public void setUserid(int userid) {
+    this.userid = userid;
+  }
+
+  public int getDominos() {
+    return dominos;
+  }
+
+  public void setDominos(int dominos) {
+    this.dominos = dominos;
+  }
+
+}
